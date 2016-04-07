29bb551b9fe6781206c15e9a0d5fbb60317aa839
Mitja Jereb <mjereb@gmail.com>
2016-04-07 13:38:16 +0200

    used dictionary

--- csi.py
+++ csi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+'''
 barva_las = ["CCAGCAATCGC", "GCCAGTGCCG", "TTAGCTATCGC"] # Črna, Rjava, Korenček
 oblika_obraza = ["GCCACGG", "ACCACAA", "AGGCCTCA"]      # Kvadraten, Okrogel, Ovalen
 barva_oci = ["TTGTGGTGGC", "GGGAGGTGGC", "AAGTAGTGAC"]  # Modra, Zelena, Rjava
@@ -18,6 +18,47 @@ if (dna.find(ziga[0]) != -1 and dna.find(ziga[1]) != -1 and dna.find(ziga[2]) !=
 elif (dna.find(matej[0]) != -1 and dna.find(matej[1]) != -1 and dna.find(matej[2]) != -1 and dna.find(matej[3]) != -1):
     print ("Bil je Matej!")
 elif (dna.find(miha[0]) != -1 and dna.find(miha[1]) != -1 and dna.find(miha[2]) != -1 and dna.find(miha[3]) != -1):
+    print ("Bil je Miha!")
+else :
+    print ("Krivca ni med osumljenci..")
+'''
+
+barva_las = {
+    "crna":"CCAGCAATCGC",
+    "rjava":"GCCAGTGCCG",
+    "korencek":"TTAGCTATCGC"
+}
+oblika_obraza = {
+    "kvadraten":"GCCACGG",
+    "okrogel":"ACCACAA",
+    "ovalen":"AGGCCTCA"
+}
+barva_oci = {
+    "modra":"TTGTGGTGGC",
+    "zelena":"GGGAGGTGGC",
+    "rjava":"AAGTAGTGAC"
+}
+spol = {
+    "moski":"TGCAGGAACTTC",
+    "zenska":"TGAAGGACCTTC"
+}
+rasa = {
+    "belec":"AAAACCTCA",
+    "crnec":"CGACTACAG",
+    "azijec":"CGCGGGCCG"
+}
+
+ziga = [spol["moski"],rasa["belec"], barva_las["korencek"], barva_oci["rjava"], oblika_obraza["okrogel"]]
+matej = [ spol["moski"], rasa["belec"], barva_las["crna"], barva_oci["modra"], oblika_obraza["ovalen"]]
+miha = [ spol["moski"], rasa["belec"], barva_las["rjava"], barva_oci["zelena"], oblika_obraza["kvadraten"]]
+
+dna = open("dna.txt", "r").read()
+
+if (dna.find(spol["moski"]) != -1 and dna.find(rasa["belec"]) != -1 and dna.find(barva_las["korencek"]) != -1 and dna.find(barva_oci["rjava"]) != -1) and dna.find(oblika_obraza["okrogel"]) != -1:
+    print ("Bil je Žiga!")
+elif (dna.find(spol["moski"]) != -1 and dna.find(rasa["belec"]) != -1 and dna.find(barva_las["crna"]) != -1 and dna.find(barva_oci["modra"]) != -1 and dna.find(oblika_obraza["ovalen"]) != -1):
+    print ("Bil je Matej!")
+elif (dna.find(spol["moski"]) != -1 and dna.find(rasa["belec"]) != -1 and dna.find(barva_las["rjava"]) != -1 and dna.find(barva_oci["zelena"]) != -1 and dna.find(oblika_obraza["kvadraten"]) != -1):
     print ("Bil je Miha!")
 else :
     print ("Krivca ni med osumljenci..")
